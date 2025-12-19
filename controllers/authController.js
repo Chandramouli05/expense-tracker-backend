ad81940fe60b470fbd07869c024338ca904529e8
Dharshini R <chandraamoulee@gmail.com>
2025-12-19 19:26:35 +0530

    commit

---
+++ controllers/authController.js
@@ -0,0 +1,47 @@
+const User = require("../models/UserModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.signup = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "User Already Exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+    res.status(201).json({ message: "user registered successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Signup Failed" });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not Found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid password" });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: 'Login Failed' });
+  }
+};
--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const expenseRoutes = require("./routes/expenseRoutes");
-const signupRoutes = require("./routes/signupRoutes");
-const loginRoutes = require("./routes/loginRoutes");
+const authRoutes = require("./routes/authRoutes")
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/expenses", expenseRoutes );
-app.use("/api/signup", signupRoutes);
-app.use("/api/login", loginRoutes)
+app.use("/api/auth", authRoutes);
+
 
 const PORT = process.env.PORT || 3500;
 
