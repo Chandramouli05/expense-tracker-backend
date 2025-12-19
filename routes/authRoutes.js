54c898c33dc62c07b822e204800b20b90e3f9d39
Dharshini R <chandraamoulee@gmail.com>
2025-12-19 21:25:43 +0530

    commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ const controller = require("../controllers/authController");
 
 router.post("/signup", controller.signup);
 router.post("/login", controller.login);
+router.get("/getuser", controller.getUser);
 
 module.exports = router;
